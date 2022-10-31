3ef8a6ec834b53ff8bac74d1ac1d6fd93d35f711
syahrulawaludin <syahrulawaludin72@gmail.com>
2022-10-31 12:53:15 +0700

    this keyword - final

---
+++ This Keyword/This Keyword/src/Person.java
@@ -0,0 +1,42 @@
+public class Person {
+    
+    String name;
+    String address;
+    final String  country = "Indonesia";
+
+
+    // This Keyword
+    /*
+     * Saat membuat kode di dalam block constructor atua methid di dalam class, kita bisa memnggunakan kata kunci this untuk mengakses object saat ini
+     * Misal kadang kita butuh mengakses sebuah field yang  namanya sama dengan parameter method, hal ini tidak bisa dilakukan jika langsung  menyebut nama field, kita bisa mengakses nama field  tersebut dengan kata kunci this
+     * This juga tidak hanya digunakan untuk mengakses field miliki object saat ini, namun juga bisa digunakan untuk mengakses method
+     * This bisa digunakan  untuk mengatasi masalah variable shadowing
+     */
+    // CONSTRUCTOR
+    Person(String name, String address){
+    this.name = name;
+    this.address = address;
+
+    }
+    Person(String paramName){
+        this(paramName, null);
+    
+        }
+    
+    Person(){
+        this(null);
+
+    }
+    // Methos sayhello
+    void sayHello(String paramName){
+        System.out.println("Hello " + name + ", My name is " + this.name);
+
+    }
+
+    // Method sayGoodBye 
+    void sayGoodBye(String paramName){
+        System.out.println("Hello " + name + ", See you " + this.name);
+
+    }
+
+}
