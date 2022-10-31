834655ec08052fb339bf0c63de030a7d26d55ffd
syahrulawaludin <syahrulawaludin72@gmail.com>
2022-10-31 14:59:17 +0700

    Method override

---
+++ Method Overriding/Method Overriding/src/vicePresident.java
@@ -0,0 +1,8 @@
+
+// Class child dari class parent Manager
+class vicePresident extends Manager{
+    
+    void sayHelloo(String name){
+        System.out.println("Hi " + name + ", My Name Is VP " + this.name);
+    }
+}
